d8a9fdb5173912c30e6f65cb70e8e3f380bfd17d
Steven Yau <social@stevenyau.co.uk>
2019-07-13 14:32:56 +0100

    Renamed prefabs of what used to be called *Controllers to *Scenes and updated the scenes prefab instances

---
+++ Assets/TankGame/Scripts/Controllers/LeaderboardController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TankGame {
+    public class LeaderboardController : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
