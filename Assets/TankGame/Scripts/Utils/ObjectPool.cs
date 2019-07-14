84b5d455ced98f694648f3a2f1fdc2afb760f2a4
Steven Yau <social@stevenyau.co.uk>
2019-07-14 22:28:16 +0100

    Added basic object pool class

---
+++ Assets/TankGame/Scripts/Utils/ObjectPool.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame {
+    public class ObjectPool {
+        private List<GameObject> _pool;
+        private GameObject _prefab;
+        private GameObject _parent;
+        
+
+        public ObjectPool(GameObject prefab, GameObject parent, int startingCount) {
+            _prefab = prefab;
+            _parent = parent;
+            
+            _pool = new List<GameObject>();
+            for (int i = 0; i < startingCount; ++i) {
+                var obj = GameObject.Instantiate(_prefab, _parent.transform, false);
+                obj.SetActive(false);
+                _pool.Add(obj);
+            }
+        }
+
+
+        public GameObject GetObject() {
+            if (_pool.Count == 0) {
+                return GameObject.Instantiate(_prefab, _parent.transform, false);
+            } else {
+                var obj = _pool[0];
+                _pool.RemoveAt(0);
+
+                return obj;
+            }
+        }
+
+
+        public void ReturnObject(GameObject obj) {
+            obj.transform.SetParent(_parent.transform);
+            obj.SetActive(false);
+            _pool.Add(obj);
+        }
+    }
+}
