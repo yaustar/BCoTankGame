b2522ffadf773c7dcb648347caa8d25b7b5520b8
Steven Yau <social@stevenyau.co.uk>
2019-07-15 19:20:16 +0100

    Add the rotation of the tank based on movement direction and centered the pivot point to the middle of the turret

--- Assets/TankGame/Scripts/World/Tank.cs
+++ Assets/TankGame/Scripts/World/Tank.cs
@@ -6,9 +6,12 @@ namespace TankGame {
         [SerializeField]
         private float _maxSpeed;
 
+        [SerializeField]
+        private Animator _animator;
+        
 
         private ITankInput _input;
-
+        private bool _movingLastFrame = false; 
 
         private void Awake() {
             _input = GetComponent<ITankInput>();
@@ -19,35 +22,57 @@ namespace TankGame {
             if (_input != null) {
                 var direction = _input.GetDirection();
                 var moveDelta = new Vector3();
+                var trans = transform;
+
+                var localRot = trans.localEulerAngles;
                 
                 switch (direction) {
                     case InputDirection.Down: {
-                        moveDelta.y = -1f;        
+                        moveDelta.y = -1f;
+                        localRot.z = 180f;        
                         break;
                     }
                         
                     case InputDirection.Up: {
                         moveDelta.y = 1f;
+                        localRot.z = 0f;
                         break;
                     }
                     
                     case InputDirection.Left: {
                         moveDelta.x = -1f;
+                        localRot.z = 90f;
                         break;
                     }
                     
                     case InputDirection.Right: {
-                        moveDelta.x = 1f;        
+                        moveDelta.x = 1f;
+                        localRot.z = 270f;
                         break;
                     }
                     
                     default: break;
                 }
-
-                var trans = transform;
+                
                 var pos = trans.position;
                 pos += (moveDelta * (_maxSpeed * Time.deltaTime));
                 trans.position = pos;
+
+                trans.localEulerAngles = localRot;
+
+                if (direction != InputDirection.None) {
+                    if (!_movingLastFrame) {
+                        _animator.Play("Moving");
+                    }
+                    
+                    _movingLastFrame = true;
+                } else {
+                    if (_movingLastFrame) {
+                        _animator.Play("Idle");
+                    }
+                    
+                    _movingLastFrame = false;
+                }
             }
         }
     }
